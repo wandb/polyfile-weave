b2f608382610a5374a0a489eba4ede1474504c8c
zbirenbaum <zachary.birenbaum@wandb.com>
2025-11-20 10:27:32 -0800

    lower bound pin for security

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "pyreadline3;platform_system=='Windows'",
         "pyyaml>=3.13",
         "setuptools>=80.9.0",
-        "pdfminer.six"
+        "pdfminer.six>=20220524"
     ],
     extras_require={
         'demangle': ['cxxfilt'],
